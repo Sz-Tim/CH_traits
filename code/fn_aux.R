021c8672be69bf8032590777a9fa13e24ab48712
Sz-Tim <tim.szewczyk@gmail.com>
2020-03-11 09:46:03 +0100

    functions for munging data, plotting, etc

---
+++ code/fn_aux.R
@@ -0,0 +1,177 @@
+# Auxilliary functions for keeping things cleaner
+
+
+
+
+#' LAS measurements: Aggregate measurements for a single worker
+#' 
+#' @param f_LAS file name of LAS .xslx report, including directory
+#' @return Dataframe with a row for each measurement and columns for Trait
+#'   (numeric), Value (mm), and Worker
+
+aggregate_measurements <- function(f_LAS) {
+  map2_dfr(f_LAS, grep("Profile", excel_sheets(f_LAS), value=T),
+           ~read_xlsx(.x, .y, skip=1, progress=F,
+                      col_types=rep(c("numeric", "skip"), each=2)) %>% 
+             filter(row_number() %in% c(1,n())) %>% 
+             mutate(unit=str_sub(names(.)[1], -3, -2)) %>% 
+             setNames(c("x", "y", "unit")) %>%
+             summarise(Value=sqrt(diff(x)^2 + diff(y)^2) %>%
+                         units::as_units(., first(unit)) %>%
+                         units::set_units(., "mm") %>%
+                         as.numeric) %>%
+             mutate(Worker=str_sub(.x, -6, -6)), 
+           .id="Trait") 
+}
+
+
+
+
+
+
+
+#' Load and aggregate all measurements (LAS, color) within specified directories
+#'
+#' @param ant_i dataframe with ant species, covariates, etc for each tube number
+#' @param msr_dir directory containing LAS xlsx reports; recursive search
+#' @param col_dir directory containing color histograms; recursive search
+#' @param lat_names vector of trait names for lateral view
+#' @param fro_names vector of trait names for frontal view
+#' @param dor_names vector of trait names for dorsal view
+#' @return List of two data frames: wkr.df = worker-level traits, clny.df =
+#'   colony-level mean and sd values for each trait. Each is joined by all
+#'   columns from ant_i, but only tubes with trait measurements are included
+
+load_traits <- function(ant_i, msr_dir, col_dir,
+                        lat_names=c("WebersLength", "HindTibia", "MidTibia"),
+                        fro_names=c("HeadLength", "HeadWidth", 
+                                    "InterocularDistance", "ScapeLength"),
+                        dor_names=c("MesosomaWidth", "MesosomaLength", 
+                                    "HindFemur", "MidFemur")) {
+  # setup
+  msr.tubes <- dir(msr_dir, "^999", include.dirs=T, recursive=T)
+  col.tubes <- dir(col_dir, "^999", include.dirs=T, recursive=T)
+  fro_all <- lat_all <- dor_all <- vector("list", length(msr.tubes))
+  
+  pb <- txtProgressBar(min=1, max=length(msr.tubes)+length(col.tubes))
+  for(tube in seq_along(msr.tubes)) {
+    setTxtProgressBar(pb, tube)
+    
+    # files to read
+    tubeSubDir <- msr.tubes[tube]
+    tubeNo <- str_split(tubeSubDir, "/")[[1]] %>% last()
+    f_fro <- dir(paste0(msr_dir, tubeSubDir), "fro[0-9]\\.xlsx", full.names=T)
+    f_lat <- dir(paste0(msr_dir, tubeSubDir), "lat[0-9]\\.xlsx", full.names=T)
+    f_dor <- dir(paste0(msr_dir, tubeSubDir), "dor[0-9]\\.xlsx", full.names=T)
+    
+  
+    if(length(f_fro) > 0) {
+      fro_all[[tube]] <- map_dfr(f_fro, aggregate_measurements) %>%
+        mutate(TubeNo=tubeNo, unit="mm", Trait=fro_names[as.numeric(Trait)])
+    }
+    
+    if(length(f_lat) > 0) {
+      lat_all[[tube]] <- map_dfr(f_lat, aggregate_measurements) %>%
+        mutate(TubeNo=tubeNo, unit="mm", Trait=lat_names[as.numeric(Trait)])
+    }
+    
+    if(length(f_dor) > 0) {
+      dor_all[[tube]] <- map_dfr(f_dor, aggregate_measurements) %>%
+        mutate(TubeNo=tubeNo, unit="mm", Trait=dor_names[as.numeric(Trait)])
+    }
+  }
+  col.ls <- vector("list", length(col.tubes))
+  for(tube in seq_along(col.tubes)) {
+    setTxtProgressBar(pb, tube+length(msr.tubes))
+    tubeNo <- col.tubes[tube]
+    nWorkers <- length(dir(paste0(col_dir, tubeNo), "meso[1-9].txt"))
+    meso.hist <- vector("list", length=nWorkers)
+    for(i in 1:nWorkers) {
+      meso_i.f <- dir(paste0(col_dir, tubeNo, "/meso", i, " Data/"), full.names=T)
+      if(length(meso_i.f) > 0) {
+        meso.hist[[i]] <- map(meso_i.f, ~t(as.matrix(read.csv(., header=F)))) %>%
+          Reduce('+', .) %>%
+          as.data.frame(.) %>% setNames(., c("R", "G", "B")) %>%
+          mutate(value=1:256, 
+                 TubeNo=str_split_fixed(col.tubes[tube], "/", 3)[3], 
+                 Worker=as.character(i)) 
+      }
+    }
+    col.ls[[tube]] <- rbind(do.call('rbind', meso.hist))
+  }
+  close(pb)
+  
+  col.wkr <- do.call('rbind', col.ls) %>%
+    gather(channel, count, 1:3) %>%
+    group_by(TubeNo, Worker, channel) %>%
+    summarise(mn=sum(value*count)/sum(count),
+              med=median(rep(value, count)), 
+              sd=sd(rep(value, count))) %>% 
+    ungroup %>% pivot_wider(names_from=channel, values_from=c(mn, med, sd)) %>%
+    mutate(rgbCol=paste0("rgb(", med_R/256, ",", med_G/256, ",", med_B/256, ")"),
+           h=rgb2hsv(med_R, med_G, med_B, maxColorValue=255)[1,],
+           s=rgb2hsv(med_R, med_G, med_B, maxColorValue=255)[2,],
+           v=rgb2hsv(med_R, med_G, med_B, maxColorValue=255)[3,])
+  col.clny <- col.wkr %>% group_by(TubeNo) %>%
+    summarise(R=mean(med_R), G=mean(med_G), B=mean(med_B), 
+              v_var=var(v), v_CV=sd(v)/mean(v)) %>%
+    mutate(rgbCol=paste0("rgb(", R/256, ",", G/256, ",", B/256, ")"),
+           h=rgb2hsv(R, G, B, maxColorValue=255)[1,],
+           s=rgb2hsv(R, G, B, maxColorValue=255)[2,],
+           v=rgb2hsv(R, G, B, maxColorValue=255)[3,]) 
+  
+  traits.df <- rbind(do.call("rbind", fro_all), 
+                     do.call("rbind", lat_all), 
+                     do.call("rbind", dor_all)) %>%
+    full_join(., col.wkr, by=c("TubeNo", "Worker")) %>%
+    left_join(., ant_i, by="TubeNo")
+  colony.df <- traits.df %>% group_by(TubeNo, Trait) %>% 
+    summarise(mnValue=mean(Value),
+              sdValue=sd(Value)) %>%
+    full_join(., col.clny, by=c("TubeNo")) %>%
+    left_join(., ant_i, by="TubeNo")
+  
+  return(list(wkr.df=traits.df, clny.df=colony.df))
+}
+
+
+
+
+
+
+
+
+#' Plot of species-standardized color against a chosen variable
+#'
+#' @param tr.df dataframe with color values; currently 'all.sum.wide' in munge_color.R
+#' @param x_var character for which column to plot on the x-axis 
+#' @param xlab optional x-axis label
+#' @return Plot of x_var on x-axis and brightness standardized within each
+#'   species as (v - mean(v))/sd(v), with solid points for colonies and hollow
+#'   points for individual workers, colour as the actual mean colour, simple
+#'   linear regression best-fit lines (dotted = species, solid = total)
+
+plot_colors_by_v <- function(tr.df, x_var, xlab=NULL) {
+  library(tidyverse)
+  if(is.null(xlab)) xlab <- x_var
+  sp.df <- tr.df %>% group_by(SPECIESID) %>% mutate(v_std=(v-mean(v))/sd(v))
+  tube.df <- sp.df %>%  group_by(TubeNo) %>% 
+    summarise_if(is.numeric, mean)
+  reg_sp <- sp.df %>% summarise(n=n_distinct(TubeNo)) %>% filter(n > 2)
+  plot(sp.df[[x_var]], sp.df$v_std, 
+       pch=1, cex=1, col=rgb(sp.df$med_R, sp.df$med_G, sp.df$med_B, maxColorValue=255),
+       xlab=xlab, 
+       ylab=expression(Standardized~brightness:~frac(v-bar(v[s]),sigma[v[s]])))
+  points(tube.df[[x_var]], tube.df$v_std, pch=19, cex=1.5, 
+         col=rgb(tube.df$med_R, tube.df$med_G, tube.df$med_B, maxColorValue=255))
+  abline(lm(sp.df$v_std ~ sp.df[[x_var]]), lwd=2)
+  for(i in 1:n_distinct(reg_sp$SPECIESID)) {
+    sp_i.df <- filter(sp.df, SPECIESID==reg_sp$SPECIESID[i])
+    clip(range(sp_i.df[[x_var]], na.rm=T)[1], 
+         range(sp_i.df[[x_var]], na.rm=T)[2], 
+         range(sp_i.df$v_std)[1], range(sp_i.df$v_std)[2])
+    abline(lm(sp_i.df$v_std ~ sp_i.df[[x_var]]), lty=2)
+  }
+}
+
+
