cf26f6fa144c3385d7a2fd8a0e8a76f9c63c62ec
Sz-Tim <tim.szewczyk@gmail.com>
2020-11-24 11:44:49 +0100

    printing status

--- data/update_data.sh
+++ data/update_data.sh
@@ -2,6 +2,6 @@
 
 cd /Volumes/BOOTCAMP/Users/tsz/Desktop/opfo_images/
 
-rsync -R */*/*/*/dor[1-9]_Multifocus*.tif /Users/tsz/Documents/unil/opfo_main/4_traits/data/img/
+rsync -R -P */*/*/*/dor[1-9]_Multifocus*.tif /Users/tsz/Documents/unil/opfo_main/4_traits/data/img/
 
-rsync -R */*/*/[a-z][a-z][a-z][1-4].xlsx /Users/tsz/Documents/unil/opfo_main/4_traits/data/img/
+rsync -R -P */*/*/[a-z][a-z][a-z][1-4].xlsx /Users/tsz/Documents/unil/opfo_main/4_traits/data/img/
